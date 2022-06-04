b82c273073f473d5b9ac270732620ff0a87bd697
YahiroRyo <honakafn@gmail.com>
2022-06-04 15:41:42 +0900

    既読機能作成

--- client/app/Models/Mail.php
+++ client/app/Models/Mail.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Trait\HasUlid;
 
 class Mail extends Model
@@ -27,7 +28,7 @@ class Mail extends Model
     {
         return $this->hasOne(MailActive::class, 'mail_id', 'mail_id');
     }
-    public function readed()
+    public function read()
     {
         return $this->hasOne(MailRead::class, 'mail_id', 'mail_id');
     }
@@ -36,14 +37,18 @@ class Mail extends Model
     {
         return Mail::where('user_id', auth()->id())
             ->has('active')
-            ->with('readed')
             ->get();
     }
     public static function find_one(string $mail_id)
     {
-        return Mail::where('user_id', auth()->id())
-                    ->has('active')
-                    ->with('readed')
-                    ->find($mail_id);
+        return DB::transaction(function () use ($mail_id) {
+            $mail = Mail::where('user_id', auth()->id())
+                        ->has('active')
+                        ->find($mail_id);
+            MailRead::updateOrCreate([
+                'mail_id' => $mail_id
+            ]);
+            return $mail;
+        });
     }
 }
