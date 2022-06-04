303f155ec3bdf3270a29a7eeff571d0e45c75327
YahiroRyo <honakafn@gmail.com>
2022-06-04 15:41:42 +0900

    既読機能作成

--- client/resources/views/mails/list.blade.php
+++ client/resources/views/mails/list.blade.php
@@ -6,7 +6,10 @@
 <ul>
     @foreach ($mails as $mail)
         <li>
-            <a href="/users/mails/mail?mail_id={{ $mail['mail_id'] }}" class="flex align-center gap-10 p-5 border-gray-200 border-2 bg-white hover:bg-gray-200 {{ $mail['readed'] ? 'bg-gray-200' : '' }}">
+            <a
+                href="/users/mails/mail?mail_id={{ $mail['mail_id'] }}"
+                class="flex align-center gap-10 p-5 border-gray-200 border-2 bg-white hover:bg-gray-200 {{ $mail['read'] ? 'bg-gray-200' : '' }}"
+            >
                 <span class="font-bold text-gray-700">{{$mail['content']['created_at']}}</span>
                 <p class="font-bold text-gray-700">{{$mail['content']['subject']}}</p>
             </a>
