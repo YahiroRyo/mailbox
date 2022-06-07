253fdd0af5d2067732c9d4a17b7a3ab725790371
YahiroRyo <honakafn@gmail.com>
2022-06-07 14:13:10 +0900

    メールの送信を可能にした

---
+++ client/app/Http/Controllers/MailSendController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\MailSendRequest;
+use App\Models\Mail;
+use App\Models\User;
+use App\Services\MailService;
+use Aws\Ses\Exception\SesException;
+use Aws\Ses\SesClient;
+use Carbon\Carbon;
+
+class MailSendController extends Controller
+{
+    private $mail_service;
+    public function __construct()
+    {
+        $this->mail_service = new MailService();
+    }
+    public function view()
+    {
+        return view('mails.send');
+    }
+
+    public function mail_send(MailSendRequest $request)
+    {
+        $validated = $request->validated();
+        $email = auth()->user()->email;
+        $charset = 'UTF-8';
+
+        try {
+            $client = new SesClient([
+                'region' => 'us-east-1',
+                'version' => '2010-12-01',
+            ]);
+            $client->sendEmail([
+                'Destination' => [
+                    'ToAddresses' => [$validated['to_addresses']],
+                ],
+                'ReplyToAddresses' => [$email],
+                'Source' => $email,
+                'Message' => [
+                    'Body' => [
+                        'Text' => [
+                            'Charset' => $charset,
+                            'Data' => $validated['body'],
+                        ],
+                    ],
+                    'Subject' => [
+                        'Charset' => $charset,
+                        'Data' => $validated['subject'],
+                    ]
+                ]
+            ]);
+
+            $this->mail_service->mail_create(
+                auth()->id(),
+                $validated['subject'],
+                $validated['body'],
+                '',
+                $email,
+                $email,
+                '',
+                Carbon::now()->toString(),
+                true
+            );
+        } catch (SesException $e) {
+            return view('mails.send', [
+                'error_message' => '不明なエラーが発生しました。<br/>フォームの内容をもう一度確認の上送信ください。'
+            ]);
+        }
+        return redirect('/users/mails');
+    }
+}
---
+++ client/app/Http/Requests/MailSendRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MailSendRequest extends FormRequest
+{
+    public function attributes()
+    {
+        return [
+            'to_addresses' => '送信先',
+            'subject' => '件名',
+            'body' => 'メッセージ'
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'to_addresses' => ['required', 'email'],
+            'subject' => ['max:256'],
+            'body' => ['max:384000'],
+        ];
+    }
+}
