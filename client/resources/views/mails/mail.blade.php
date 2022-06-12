bc4d8c6e81e34a17ba7a7d8c91516234d642dbee
YahiroRyo <honakafn@gmail.com>
2022-06-12 15:25:43 +0900

    受信表示バグ修正

--- client/resources/views/mails/mail.blade.php
+++ client/resources/views/mails/mail.blade.php
@@ -13,7 +13,7 @@
         <a class="text-gray-900" href="{{url()->previous()}}">← 戻る</a>
         <div class="flex items-center gap-2 mt-5">
             <img class="w-5" src="{{ asset('/images/clock.png') }}" alt="時間">
-            <span class="text-gray-900 text-base">{{\Carbon\Carbon::parse($mail['profile']['receive_user']['mail_created_at'])->format('Y年m月d日H時m分')}}</span>
+            <span class="text-gray-900 text-base">{{$mail['profile']['mail_created_at']}}</span>
         </div>
         <h2 class="text-gray-700 font-bold text-2xl mt-5">送信元</h2>
         <p class="text-gray-700 mt-1">{{$mail['profile']['receive_user']['name']}}</p>
